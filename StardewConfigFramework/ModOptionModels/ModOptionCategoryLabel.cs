9ddd35bd42091242de1182667422eb41c6eebcf8
Justin Oroz <justinoroz@me.com>
2018-06-10 16:51:12 -0700

    Removed Framework

--- StardewConfigFramework/ModOptionModels/ModOptionCategoryLabel.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace StardewConfigFramework {
-	public class ModOptionCategoryLabel: ModOption {
-		public ModOptionCategoryLabel(string identifier, string label) : base(identifier, label) {
-
-		}
-	}
-}
