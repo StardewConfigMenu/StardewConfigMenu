7ff80e6226d13befdf30a543fc2d3073f28caca5
Justin Oroz <justinoroz@me.com>
2018-07-08 10:30:05 -0700

    Created ConfigTextfield

---
+++ StardewConfigMenu/Menu/Components/ConfigTextfield.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewConfigFramework.Options;
+
+namespace StardewConfigMenu.Components {
+	sealed class ConfigTextfield: SCMControl {
+		readonly IConfigString ModData;
+
+		public ConfigTextfield(IConfigString option) : base(option.Label, option.Enabled) {
+			ModData = option;
+		}
+	}
+}
